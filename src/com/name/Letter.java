ab34bf9c9acbcd2e2372f5e7d69bd7c8a9cd85e4
Nitisha <nitishak10@gmail.com>
2021-05-13 17:16:04 -0400

    Initial commit

---
+++ src/com/name/Letter.java
@@ -0,0 +1,18 @@
+package com.name;
+
+public class Letter {
+
+    public static void main(String[] args) {
+        System.out.println("        **                **");
+        System.out.println("       ** **             **");
+        System.out.println("      **   **           **");
+        System.out.println("     **     **         **");
+        System.out.println("    **       **       **");
+        System.out.println("   **         **     **");
+        System.out.println("  **           **   **");
+        System.out.println(" **             ** **");
+        System.out.println("**               **");
+
+    }
+}
+
